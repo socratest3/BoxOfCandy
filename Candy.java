973e144a460adbc94c91ae6ebc036dff65fd3149
socratest3 <socratest3@nycstudents.net>
2025-04-02 11:01:16 -0400

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,1 +1,16 @@
-
+public class Candy 
+{
+    private String flavor;
+    public Candy(String f)
+    {
+        flavor = f;
+    }
+    public String toString()
+    {
+        return flavor;
+    }
+       public String getFlavor()
+       {
+        return flavor;
+       }
+    }
